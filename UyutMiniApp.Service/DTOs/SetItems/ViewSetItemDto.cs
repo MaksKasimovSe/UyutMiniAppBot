b1be57a02776f086e3798d4dcb27e427a0a33121
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-07-18 01:41:27 +0900

    Set items view resolved

--- UyutMiniApp.Service/DTOs/SetItems/ViewSetItemDto.cs
+++ UyutMiniApp.Service/DTOs/SetItems/ViewSetItemDto.cs
@@ -5,6 +5,7 @@ public class ViewSetItemDto
 {
     public Guid Id { get; set; }
     public Guid MenuItemId { get; set; }
+    public Guid IncludedItemId { get; set; }
     public bool IsReplaceable { get; set; }
 
     public ICollection<ViewSetItemReplacementOptionDto> ReplacementOptions { get; set; }
