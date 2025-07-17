447c64248b9de9b8af8da205bf04a49d472c7daf
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-07-17 19:56:52 +0900

    Added menuitem id

--- UyutMiniApp.Service/DTOs/SetItems/UpdateSetItemDto.cs
+++ UyutMiniApp.Service/DTOs/SetItems/UpdateSetItemDto.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using UyutMiniApp.Service.DTOs.SetItemReplacementOptions;
 
 namespace UyutMiniApp.Service.DTOs.SetItems;
 
@@ -6,6 +7,8 @@ public class UpdateSetItemDto
 {
     public Guid IncludedItemId { get; set; }
     public bool IsReplaceable { get; set; }
+
+    public ICollection<CreateSetItemReplacementOptionDto> ReplacementOptions { get; set; }
     [JsonIgnore]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
--- UyutMiniApp.Service/DTOs/SetItems/ViewSetItemDto.cs
+++ UyutMiniApp.Service/DTOs/SetItems/ViewSetItemDto.cs
@@ -1,8 +1,11 @@
+using UyutMiniApp.Service.DTOs.SetItemReplacementOptions;
+
 namespace UyutMiniApp.Service.DTOs.SetItems;
 public class ViewSetItemDto
 {
     public Guid Id { get; set; }
     public Guid MenuItemId { get; set; }
-    public Guid IncludedItemId { get; set; }
     public bool IsReplaceable { get; set; }
+
+    public ICollection<ViewSetItemReplacementOptionDto> ReplacementOptions { get; set; }
 }
