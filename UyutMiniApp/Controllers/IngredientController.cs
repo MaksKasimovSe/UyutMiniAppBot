f26a8de2e7937287eb031a69faa070acfbba98ac
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-07-18 19:11:59 +0900

    Added auth for ingredientcontroller

--- UyutMiniApp/Controllers/IngredientController.cs
+++ UyutMiniApp/Controllers/IngredientController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using UyutMiniApp.Service.DTOs.Ingredients;
 using UyutMiniApp.Service.Interfaces;
 
@@ -7,11 +8,11 @@ namespace UyutMiniApp.Controllers
     [ApiController, Route("[controller]")] 
     public class IngredientController(IIngredientService ingredientService) : ControllerBase
     {
-        [HttpPost]
+        [HttpPost, Authorize(Roles = "Admin")]
         public async Task AddAsync(CreateIngredientDto dto) => 
             await ingredientService.CreateAsync(dto);
 
-        [HttpPut("id")]
+        [HttpPut("id"), Authorize(Roles = "Admin")]
         public async Task UpdateAsync([FromRoute]Guid id, UpdateIngredientDto dto) =>
             await ingredientService.UpdateAsync(id, dto);
 
@@ -19,7 +20,7 @@ namespace UyutMiniApp.Controllers
         public async Task<IActionResult> GetAsync([FromRoute]Guid id) =>
             Ok(await ingredientService.GetAsync(id));
 
-        [HttpDelete("id")]
+        [HttpDelete("id"), Authorize(Roles = "Admin")]
         public async Task DeleteAsync([FromRoute] Guid id) =>
             await ingredientService.DeleteAsync(id);
 
