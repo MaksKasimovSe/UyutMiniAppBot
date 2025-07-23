24d567b8c700ec545862bdb7c23980628ffde7c4
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-07-23 15:17:30 +0900

    Migration changes

---
+++ UyutMiniApp.Data/Migrations/20250723040755_ReceiptImageMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UyutMiniApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReceiptImageMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrderUrl",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderUrl",
+                table: "Orders");
+        }
+    }
+}
