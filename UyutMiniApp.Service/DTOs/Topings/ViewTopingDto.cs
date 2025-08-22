7db78b9afa82372ef24fd5ce1271f983482ebad4
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-08-22 20:18:56 +0900

    Changes

--- UyutMiniApp.Service/DTOs/Topings/ViewTopingDto.cs
+++ UyutMiniApp.Service/DTOs/Topings/ViewTopingDto.cs
@@ -3,6 +3,7 @@
     public class ViewTopingDto
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
         public decimal Price { get; set; }
         public Guid MenuItemId { get; set; }
     }
