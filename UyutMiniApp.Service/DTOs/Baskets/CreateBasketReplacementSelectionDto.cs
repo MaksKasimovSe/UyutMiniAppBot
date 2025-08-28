4771126d356f5cc50d7622c88677513036e0f19c
MuhammadKasimov <muhammadmamushah.com@gmail.com>
2025-08-28 17:06:53 +0900

    Attributes for validation added

--- UyutMiniApp.Service/DTOs/Baskets/CreateBasketReplacementSelectionDto.cs
+++ UyutMiniApp.Service/DTOs/Baskets/CreateBasketReplacementSelectionDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,9 @@ namespace UyutMiniApp.Service.DTOs.Baskets
 {
     public class CreateBasketReplacementSelectionDto
     {
+        [Required]
         public Guid OriginalSetItemId { get; set; }
-
+        [Required]
         public Guid ReplacementMenuItemId { get; set; }
     }
 }
--- UyutMiniApp.Service/DTOs/SetReplacementSelections/CreateSetReplacementSelectionDto.cs
+++ UyutMiniApp.Service/DTOs/SetReplacementSelections/CreateSetReplacementSelectionDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UyutMiniApp.Service.DTOs.SetReplacementSelections;
 public class CreateSetReplacementSelectionDto
 {
     public Guid OrderItemId { get; set; }
+    [Required]
     public Guid OriginalSetItemId { get; set; }
+    [Required]
     public Guid ReplacementMenuItemId { get; set; }
 }
